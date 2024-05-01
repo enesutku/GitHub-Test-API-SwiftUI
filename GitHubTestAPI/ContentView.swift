5e49798704a1cbf266e3670923a6fde1749cbe87
Enes UTKU <ensutku@hotmail.com>
2024-05-01 19:45:15 +0300

    Relase

--- GitHubTestAPI/ContentView.swift
+++ GitHubTestAPI/ContentView.swift
@@ -3,17 +3,85 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var user: GitHubUser?
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack (spacing: 20) {
+            
+            AsyncImage(url: URL(string: user?.avatarUrl ?? "")) { image in
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .clipShape(Circle())
+            } placeholder: {
+                Circle()
+                    .foregroundStyle(.secondary)
+            }
+            .frame(width: 120, height: 120)
+            
+            Text(user?.login ?? "Login")
+                .bold()
+                .font(.title3)
+            
+            Text(user?.bio ?? "Bio")
+                .padding()
+            
+            Spacer()
         }
         .padding()
+        .task {
+            do {
+                user = try await getUser()
+            } catch GHError.invalidURL {
+                print("Invalid URL")
+            } catch GHError.invalidResponse {
+                print("Invalid Response")
+            } catch GHError.invalidData {
+                print("Invalid Data")
+            } catch {
+                print("Unexpected Error")
+            }
+        }
+    }
+    
+    func getUser() async throws -> GitHubUser {
+        let endpoint = "https://api.github.com/users/enesutku"
+        
+        guard let url = URL(string: endpoint) else {
+            throw GHError.invalidURL
+        }
+        
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+            throw GHError.invalidResponse
+        }
+        
+        do {
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            return try decoder.decode(GitHubUser.self, from: data)
+        } catch {
+            throw GHError.invalidData
+        }
+        
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct GitHubUser: Codable {
+    let login: String
+    // Normally, it needs to be avatar_url but we want to use camel case!
+    let avatarUrl: String
+    let bio: String
+}
+
+enum GHError: Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
 }
